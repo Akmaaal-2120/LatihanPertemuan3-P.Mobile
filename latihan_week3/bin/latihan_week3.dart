5dd089b8f40cec3750974d31fd610fbe5c88db6b
Akmal-001 <kitsunenpp@gmail.com>
2026-03-10 12:46:34 +0700

    memperbaiki komen di main

--- latihan_week3/bin/latihan_week3.dart
+++ latihan_week3/bin/latihan_week3.dart
@@ -1,6 +1,7 @@
 // main.dart
 void main() { 
-  List> riwayat = []; // Simulasi beberapa input hitungBMI(170, 65, riwayat);
+  List> riwayat = []; // Simulasi beberapa input 
+hitungBMI(170, 65, riwayat);
 hitungBMI(165, 75, riwayat); 
 hitungBMI(180, 85, riwayat); // Tampilkan riwayat menggunakan perulangan
 tampilkanRiwayat(riwayat);
