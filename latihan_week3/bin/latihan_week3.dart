a1c7081f275b726e719f38f40753bfe7f5fcb4ec
Akmal-001 <kitsunenpp@gmail.com>
2026-03-10 12:52:12 +0700

    Menambah fungsi history

--- latihan_week3/bin/latihan_week3.dart
+++ latihan_week3/bin/latihan_week3.dart
@@ -1,22 +1,37 @@
 // main.dart
 void main() { 
-  List> riwayat = []; // Simulasi beberapa input hitungBMI(170, 65, riwayat);
-hitungBMI(165, 75, riwayat); 
-hitungBMI(180, 85, riwayat); // Tampilkan riwayat menggunakan perulangan
-tampilkanRiwayat(riwayat);
+  List<Map<String, dynamic>> riwayat = [];
+  hitungBMI(170, 65, riwayat);
+  hitungBMI(165, 75, riwayat); 
+  hitungBMI(180, 85, riwayat); // Tampilkan riwayat menggunakan perulangan
+  tampilkanRiwayat(riwayat);
 }
 
-void hitungBMI(double tinggiCm, double beratKg, List> riwayat) { // Konversitinggi dari cm ke m 
-double tinggiM = tinggiCm / 100; // Hitung BMI 
-double bmi = beratKg / (tinggiM *tinggiM); // Tentukan kategori menggunakan percabangan 
-String kategori; 
-if (bmi < 18.5) { 
-  kategori ="Kurus"; 
-  } else if (bmi < 25) { 
-    kategori = "Normal"; 
-    } else if (bmi < 30) { 
-      kategori = "Gemuk"; 
-      } else {
-kategori = "Obesitas"; } // Simpan hasil ke riwayat 
-riwayat.add({ 'tinggi': tinggiCm, 'berat': beratKg, 'bmi': bmi, 'kategori': kategori });
+void hitungBMI(double tinggiCm, double beratKg, List<Map<String, dynamic>> riwayat) { // Konversitinggi dari cm ke m 
+  double tinggiM = tinggiCm / 100; // Hitung BMI 
+  double bmi = beratKg / (tinggiM *tinggiM); // Tentukan kategori menggunakan percabangan 
+  String kategori; 
+  if (bmi < 18.5) { 
+    kategori ="Kurus"; 
+    } else if (bmi < 25) { 
+      kategori = "Normal"; 
+      } else if (bmi < 30) { 
+        kategori = "Gemuk"; 
+        } else {
+  kategori = "Obesitas"; } // Simpan hasil ke riwayat 
+  riwayat.add({ 'tinggi': tinggiCm, 'berat': beratKg, 'bmi': bmi, 'kategori': kategori });
+}
+
+// Fungsi tambahan untuk menampilkan riwayat (menggunakan perulangan)
+void tampilkanRiwayat(List<Map<String, dynamic>> riwayat) {
+  print("=== RIWAYAT PERHITUNGAN BMI ===");
+  
+  for (var i = 0; i < riwayat.length; i++) {
+    var item = riwayat[i];
+    print("Data ke-${i + 1}:");
+    print("Tinggi: ${item['tinggi']}cm | Berat: ${item['berat']}kg");
+    // .toStringAsFixed(1) agar angka di belakang koma tidak terlalu panjang
+    print("BMI: ${item['bmi'].toStringAsFixed(1)} (${item['kategori']})");
+    print("-" * 30);
+  }
 }
